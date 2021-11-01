40de3498423533128758f901687867939b8a3c25
MaryJoP10 <maryjop10@gmail.com>
2021-11-01 00:14:19 -0500

    Paginas

---
+++ Frontend/src/Pages/request.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Request = () =>{
+    return(
+        <div style={{color: 'white'}}>
+            <h1>Request</h1>
+        </div>
+    )
+}
+
+export default Request

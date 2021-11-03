7a3f766440dd66150ef0c1e1980d007738b65e7b
MaryJoP10 <maryjop10@gmail.com>
2021-11-03 11:32:04 -0500

    Navbar arreglada

--- Frontend/src/Components/Navbar/NavbarE.js
+++ Frontend/src/Components/Navbar/NavbarE.js
@@ -2,18 +2,20 @@ import styled from 'styled-components'
 import {NavLink as Link} from 'react-router-dom'
 
 export const Nav = styled.nav`
+    display: flex;
     background: black;
     height: 105px;
-    widht: 100%
+    width: 100%
+    position: fixed;
+    justify-content: end;
 `
 
 
 export const NavLink = styled(Link)`
 &.active>.button{
     border-radius: 10px;
-    width: 150px;
-    height: 50px;
     transition: 1.2
+    box-shadow: 3px 3px 4px #7df8ec;
 }
 
 `
